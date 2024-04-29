e43f5dace8207c81d9bb1164957a781dad676f21
SeckinMuhittin <160601399+SeckinMuhittin@users.noreply.github.com>
2024-04-29 17:18:13 +0300

    26.04 Sürüm Güncelleme
    Güncellendi

---
+++ assets/js/pages/form-wizard.init.js
@@ -0,0 +1,35 @@
+$(function(){
+    $("#vertical-wizard1").steps({headerTag:"h3",bodyTag:"section",transitionEffect:"fade",stepsOrientation:"vertical",enableAllSteps:true,enablePagination:true,saveState:true,enableKeyNavigation:true,autoFocus:true,
+    /* Labels */
+    labels: {
+        cancel: "İptal",
+        current: "şimdiki adım:",
+        pagination: "Sayfalar",
+        finish: "Son",
+        next: "İleri",
+        previous: "Geri",
+        loading: "Yükleniyor..."
+    }}),
+    $("#vertical-wizard2").steps({headerTag:"h3",bodyTag:"section",transitionEffect:"fade",stepsOrientation:"vertical",enableAllSteps:true,enablePagination:true,saveState:true,enableKeyNavigation:true,autoFocus:true,
+    /* Labels */
+    labels: {
+        cancel: "İptal",
+        current: "şimdiki adım:",
+        pagination: "Sayfalar",
+        finish: "Son",
+        next: "İleri",
+        previous: "Geri",
+        loading: "Yükleniyor..."
+    }}),
+    $("#vertical-wizard3").steps({headerTag:"h3",bodyTag:"section",transitionEffect:"fade",stepsOrientation:"vertical",enableAllSteps:true,enablePagination:true,saveState:true,enableKeyNavigation:true,autoFocus:true,
+    /* Labels */
+    labels: {
+        cancel: "İptal",
+        current: "şimdiki adım:",
+        pagination: "Sayfalar",
+        finish: "Son",
+        next: "İleri",
+        previous: "Geri",
+        loading: "Yükleniyor..."
+    }})
+});
